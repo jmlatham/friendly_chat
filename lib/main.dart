a6687a2e3155ac69439a3ad4b77d6c7befcf247a
jmlatham <jmlatham@yahoo.com>
2021-12-02 08:44:08 -0600

    Simplified main and added a ChatScreen class.

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,34 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      title: 'FriendlyChat',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('FriendlyChat'),
-        ),
-      ),
-    ),
+    FriendlyChatApp(),
   );
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Friendly Chat',
+      home:ChatScreen(),
+    );
+  }
+}
+
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FriendlyChat'),
+      ),
+    );
+  }
 }
